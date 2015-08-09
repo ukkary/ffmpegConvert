33a11a6621f990c1f87efd47754f2589d6ed692d
ukkary <ukkary@gmail.com>
2015-08-09 23:56:16 +0900

    リファクタ

--- ffmpegConvert.py
+++ ffmpegConvert.py
@@ -2,11 +2,8 @@
 #coding:utf-8
 
 import os
-import sys
 import commands
 
-
-try_flag = False
 
 def convert(url):
 	try:
@@ -14,7 +11,9 @@ def convert(url):
 		source = "/".join(ss)
 	except:
 		source = url.split(".")[:-1][0]
+		
 	callback = commands.getstatusoutput("sh ./convert.sh " + url + " " + source)
+	
  	if callback[0] == 0:
  		alert(1, url)
 		return True
@@ -22,34 +21,36 @@ def convert(url):
 		alert(3, "e")
 		return False
 	
-def get_path(path):
-	lis = []
-	subdir = []
+def get_path(base_path):
+	dir_list = []
+	sub_dir = []
 
-	for root, dir, files in os.walk(path):
+	for root, dir, files in os.walk(base_path):
 		for name in files:
-			subdir.append(os.path.join(root, name))
+			sub_dir.append(os.path.join(root, name))	
 				
-  	for d in subdir:
+  	for d in sub_dir:
   		if d.split(".")[-1] == "mp4":
-  			s = dir.split("/")[1:]
+  			s = d.split("/")[1:]
   			lis.append("/".join(s))
- 	return lis
+
+ 	return dir_list
 	
 def alert(x, name):
 	base = "terminal-notifier -title 'ffmpegConverter' -message '"
 	
 	if x == 1:
 		commands.getoutput(base + name + "  が変換されました'")
+		
 	if x == 2:
 		commands.getoutput(base + "   変換が終わりました'")	
+		
 	if x == 3:
 		commands.getoutput(base + "   変換に失敗しました'")
 		
 if __name__ == "__main__":
 	inp = get_path("input")
 	out = get_path("output")
-	argv = sys.argv
 	
  	while True:
  		out.append("e")
@@ -59,5 +60,5 @@ if __name__ == "__main__":
 	for i in range(len(inp)):
 		if inp[i] not in out:
 			convert(inp[i])
-		else:
-			alert(2, "e")
+	
+	alert(2, "e")
